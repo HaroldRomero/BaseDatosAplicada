a05ff802ca4b1f22807748cedee2ffe6316cb641
ccumaco <carlos.cumaco@inoutdelivery.com>
2020-10-08 20:15:57 -0500

    actualizacion base de datos final

--- Carlos/Primer Corte/Crear base de datos final.sql
+++ Carlos/Primer Corte/Crear base de datos final.sql
@@ -32,10 +32,16 @@ Dirección VARCHAR(30) NOT NULL
 CREATE TABLE PDV
 (Code INT NOT NULL PRIMARY KEY,
 Nombre VARCHAR(30) NOT NULL,
-ValorUnitario INT not NULL
+ValorUnitario INT not NULL,
+Active boolean not NULL
 );
 
 INSERT INTO Clientes
 VALUES ('1233451231', 'Francy', 'montero', '3203564432', 'carrera 112 calle 42');
 INSERT INTO Clientes
-VALUES ('76859476945', 'Manuel', 'Chacon', '3204564432', 'carrera 12 calle 54')
+VALUES ('76859476945', 'Manuel', 'Chacon', '3204564432', 'carrera 12 calle 54');
+
+ALTER TABLE PDV ADD Active boolean not NULL;
+
+INSERT INTO pdv
+VALUES (11, 'Pera', 3200 , true);
