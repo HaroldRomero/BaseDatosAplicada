d0a4a31cf80d20e3ad6a315395aef8a1f5c39895
ccumaco <carlos.cumaco@inoutdelivery.com>
2020-10-31 12:20:17 -0500

    Registros monica

--- ProyectoFinal.sql
+++ ProyectoFinal.sql
@@ -46,14 +46,58 @@ Id_Cliente VARCHAR(30) not NULL,
 Id_Pdv int not NULL
 );
 
-alter TABLE PDV add id_inv int not null;
+insert into Empleado
+	values("103056784","Jose Fernando","Gomez Perez","6754893","Cajero");
+insert into Empleado
+	values("578364634","Ricardo","Perez Galindo","3117856435","Domiciliario");
+insert into Empleado
+	values("53627463","Jorge Ernesto","Quiroga Gonzalez","3208765432","administrador");
+insert into Empleado
+	values("56474839","Carlos  ","Rojas Giraldo","3505647838","Preparador");
 
+insert into Clientes
+    values("6553663439", "4011", "Ricardo Jose","Perez Rojas","43536367","Cra 11 #41-15");
+insert into Clientes
+    values("866463739", "4002", "Diego Alejandro","Fernandez Romero","3115673434","Cra 67 #56-15");
+insert into Clientes
+    values("76475454", "4012", "Alejandro","Hernandez Casas","3123425674","Trans 15 #41-15");
+
+insert into Inventario
+    values("14156", "Helado", " Dulce","20000","5678");
+insert into Inventario
+    values("14203", "Fruta", " Citrica ","10000","5719");
+insert into Inventario
+    values("14304", "Galleta", "Crocante","30000","5819");
+
+insert into Proveedores
+    values("5719", "Natural Tropic", " Frutas","3214567898","Calle 16# 99-18");
+insert into Proveedores
+    values("5819", "frutiban", "Frutos secos", "3116784536" ,"Cra 14 #56-67");
+insert into Proveedores
+    values("5919", "Ferjusanz", "Helado", "3116784536" ,"Cra 14 #56-67");
+
+insert into PDV
+    values("1123", "Ensalada Natural", "2300","14156");
+insert into PDV
+    values("1143",  "Ensalada Mixta","6000","14203");
+insert into PDV
+    values("1134", "Ensalada Junior", "7000" ,"14204");
+
+insert into Factura
+    values("11239", "2020/01/13", "3","5436","4011","1123");
+insert into Factura
+    values("11430",  "2020/03/14","5","6754","4002","1143");
+insert into Factura
+    values("11234", "2020/04/15","2","8769","4012","1134");
+
+
+
+alter TABLE PDV add id_inv int not null;
 
 alter TABLE Clientes
 ADD CONSTRAINT fk_pdv
 FOREIGN KEY (id_Pdv)
 REFERENCES  PDV (Code);
-
 
 alter TABLE Factura
 ADD CONSTRAINT fk_empleado
